f09cce7ca2ade56cb85cc6e5d261a65b73606453
KAMIL <kamil@KAMILs-MacBook-Pro.local>
2023-05-16 12:25:01 +0200

    Added button, which shows models to each car

--- src/Cars.js
+++ src/Cars.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 //renders list of cars
 export default function Cars({ cars }) {
+  const [expandBrands, setExpandedBrands] = useState([]);
+
+  const handlButtonClick = (brand) => {
+    if (expandBrands.includes(brand)) {
+      setExpandedBrands((prev) => prev.filter((item) => item !== brand));
+    } else {
+      setExpandedBrands((prev) => [...prev, brand]);
+    }
+  };
+
   return cars.map((car, index) => {
     return (
       <div key={index}>
-        {car.models.map((model, index) => (
-          <div key={index}>
-            {car.brand} {model}
-          </div>
-        ))}
+        {car.brand}{" "}
+        <button onClick={() => handlButtonClick(car.brand)}>
+          {expandBrands.includes(car.brand) ? "Zwiń modele" : "Rozwiń modele"}
+        </button>
+        <ul>
+          {expandBrands.includes(car.brand) &&
+            car.models.map((model, index) => (
+              <li key={index}>
+                {car.brand} {model}
+              </li>
+            ))}
+        </ul>
       </div>
     );
   });
